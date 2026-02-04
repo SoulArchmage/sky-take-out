a67a1cc632a6d6eb187f218ac079c72cf2fac279
gong <xianruigong@163.com>
2026-02-04 17:37:32 +0800

    新增分页查询员工功能，并扩展了消息转换器，配置了日期时间的显示格式

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import com.sky.utils.PasswordUtils;
 import org.springframework.beans.BeanUtils;
@@ -61,7 +65,6 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
-    @Override
     public void add(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
 
@@ -83,6 +86,15 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
+    }
+
+    public PageResult page(EmployeePageQueryDTO employeePageQueryDTO) {
+        // 1.开始分页查询，设置分页参数
+        PageHelper.startPage(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
+        // 2.执行查询
+        Page<Employee> page = employeeMapper.pageQuery(employeePageQueryDTO);
+        // 3.封装分页结果并返回
+        return new PageResult(page.getTotal(), page.getResult());
     }
 
 }
