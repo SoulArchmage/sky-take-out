3225c00f6866865ad0a68a619a501952402931e9
gong <xianruigong@163.com>
2026-02-05 21:37:30 +0800

    新增了菜品修改、删除、根据id查询等功能

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,8 +1,12 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * ClassName: DishFlavorMapper
@@ -15,7 +19,24 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface DishFlavorMapper {
-    @Insert("insert into dish_flavor(id, dish_id, name, value) values (default,#{dishId},#{name},#{value})")
-    void insert(DishFlavor dishFlavor);
+
+    void insertBatch(List<DishFlavor> flavorList);
+
+    /**
+     * 根据菜品id删除对应的口味数据
+     * @param dishId
+     */
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteByDishId(Long dishId);
+
+    /**
+     * 根据菜品id查询对应的口味数据
+     * @param dishId
+     * @return
+     */
+    @Select("select * from dish_flavor where dish_id = #{dishId}")
+    List<DishFlavor> getByDishId(Long dishId);
+
+
 }
 
