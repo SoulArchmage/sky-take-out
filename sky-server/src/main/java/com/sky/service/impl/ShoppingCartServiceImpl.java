267865506279b1ffb93f874ba2f8b2f8fde61feb
gong <xianruigong@163.com>
2026-02-09 20:12:17 +0800

    新增地址簿相关功能

--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -5,7 +5,6 @@ import com.sky.dto.ShoppingCartDTO;
 import com.sky.entity.Dish;
 import com.sky.entity.Setmeal;
 import com.sky.entity.ShoppingCart;
-import com.sky.exception.ShoppingCartBusinessException;
 import com.sky.mapper.DishMapper;
 import com.sky.mapper.SetmealMapper;
 import com.sky.mapper.ShoppingCartMapper;
@@ -40,12 +39,6 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         ShoppingCart shoppingCart = new ShoppingCart();
         BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
         shoppingCart.setUserId(userId);
-        
-        // 参数校验：必须指定菜品或套餐
-        if (shoppingCart.getDishId() == null && shoppingCart.getSetmealId() == null) {
-            throw new ShoppingCartBusinessException("请选择菜品或套餐");
-        }
-        
         // 如果购物车中有该菜品，则数量加1
         List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
         if (list != null && !list.isEmpty()) {
@@ -61,18 +54,12 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             // 判断是菜品还是套餐
             if (shoppingCart.getDishId() != null) {
                 Dish dish = dishMapper.getById(shoppingCart.getDishId());
-                if (dish == null) {
-                    throw new ShoppingCartBusinessException("菜品不存在");
-                }
                 shoppingCart.setAmount(dish.getPrice());
                 shoppingCart.setImage(dish.getImage());
                 shoppingCart.setName(dish.getName());
             }
             else {
                 Setmeal setmeal = setmealMapper.getById(shoppingCart.getSetmealId());
-                if (setmeal == null) {
-                    throw new ShoppingCartBusinessException("套餐不存在");
-                }
                 shoppingCart.setAmount(setmeal.getPrice());
                 shoppingCart.setImage(setmeal.getImage());
                 shoppingCart.setName(setmeal.getName());
@@ -81,7 +68,6 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             shoppingCartMapper.insert(shoppingCart);
         }
     }
-
     @Override
     public void clean() {
         Long userId = BaseContext.getCurrentId();
@@ -109,4 +95,5 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
             }
         }
     }
+
 }
